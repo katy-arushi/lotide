f3e06f627e601c541c955c98b348c43a27bbc4b4
Arushi Katyal <katy.arushi@gmail.com>
2021-07-22 21:42:05 +0000

    implemented

---
+++ tail.js
@@ -0,0 +1,11 @@
+function tail(originalArray) {
+  let newArray = [];
+  for (let i = 0; i < originalArray.length; i ++) {
+    newArray.push(originalArray[i]);
+  }
+  if (originalArray.length === 0 || originalArray.length === 1) {
+    return [];
+  }
+  newArray.shift();
+  return newArray;
+}
