374499ef73fd63778a14c4585fe456cd02e2b79e
Arushi Katyal <katy.arushi@gmail.com>
2021-07-22 21:06:38 +0000

    refactor

--- head.js
+++ head.js
@@ -1,15 +1,4 @@
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 function head(myArray) {
   let element = myArray[0]
   return element
 }
-
-assertEqual(head([5]), 5);
-assertEqual(head([], undefined));
