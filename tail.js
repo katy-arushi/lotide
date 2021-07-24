bd9ad76465022b816a27afcd2f8fb53047b231f9
Arushi Katyal <katy.arushi@gmail.com>
2021-07-24 03:38:22 +0000

    fixed linting error

--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
 function tail(originalArray) {
   let newArray = [];
-  for (let i = 0; i < originalArray.length; i ++) {
+  for (let i = 0; i < originalArray.length; i++) {
     newArray.push(originalArray[i]);
   }
   if (originalArray.length === 0 || originalArray.length === 1) {
