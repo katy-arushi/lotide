3df3d8891c735ab4c3dc4534100df8359182056c
Arushi Katyal <katy.arushi@gmail.com>
2021-07-30 16:45:53 -0400

    removed test cases for countOnly

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,3 @@
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
 function countOnly(allItems, itemsToCount) {
   const results = {}
   for (const item of allItems) {
@@ -19,22 +11,3 @@ function countOnly(allItems, itemsToCount) {
   }
   return results;
 }
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
