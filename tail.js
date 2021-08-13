8b5530890f28263dc1eb1a913a210e7b97aa61b1
Arushi Katyal <katy.arushi@gmail.com>
2021-08-12 22:05:35 -0400

    .DS_Store banished!

--- tail.js
+++ tail.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 function tail(originalArray) {
   let newArray = [];
   for (let i = 0; i < originalArray.length; i++) {
@@ -8,4 +10,9 @@ function tail(originalArray) {
   }
   newArray.shift();
   return newArray;
-}
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
