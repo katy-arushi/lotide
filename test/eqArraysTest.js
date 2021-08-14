322525fa59b2040daab9b84b6da1c22c1cb16469
Arushi Katyal <katy.arushi@gmail.com>
2021-08-14 00:53:39 -0400

    adding tests and linting

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,20 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+describe("#eqArrays", () => {
+  it('should return true for [1, 2, 3], [1, 2, 3]', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  });
+
+  it('should return false for [1, 2, 3], [1, 2, 4]', () => {
+    assert.isFalse(eqArrays([1, 2, 3], [1, 2, 4]));
+  });
+
+  it('should return true for ["1", "2", "3"], ["1", "2", "3"]', () => {
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+  });
+
+  it('should return false for ["1", "2", "3"], ["1", "2", 3]', () => {
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+  });
+});
--- test/mapTest.js
+++ test/mapTest.js
@@ -3,7 +3,7 @@ const map = require('../map');
 
 describe("#map", () => {
   const words = ["ground", "control", "to", "major", "tom"];
-  it("returns ['g', 'c', 't', 'm', 't'] for ['ground', 'control', 'to', 'major', 'tom']", () => {
+  it("should return ['g', 'c', 't', 'm', 't'] for ['ground', 'control', 'to', 'major', 'tom']", () => {
     assert.deepEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
   });
 });
